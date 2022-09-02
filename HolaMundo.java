bfb08c007e8dcc8be28d8691673905d50c7187e1
Paolo Paredes <76070438+darkkings19@users.noreply.github.com>
2022-09-02 16:28:20 -0400

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,8 @@
 public class HolaMundo {
         public static void main(String[] args) {
             System.out.println("Hola Mundo");
-            System.out.println("Esteban Saez");
+            System.out.println("Esteban");
+            System.out.println("Paolo");
+            System.out.println("Thomas");
         }
 }
